d5025c82176cc72206c7b7040cbcef74e41bc23f
pbKrish <pbKrish07@gmail.com>
2018-06-05 18:30:54 +0530

    Initial commit

---
+++ Assets/enemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Move");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * 3f * Time.deltaTime); 
+	}
+
+	IEnumerator Move() {
+
+
+		while (true) {
+			yield return new WaitForSeconds (3.5f);
+			transform.eulerAngles += new Vector3 (0, 180f, 0);
+		}
+	}
+}
